81890a3038a43ed02ec0aace7529d4aaca82845d
RomuloRamos <45272564+RomuloRamos@users.noreply.github.com>
2019-01-04 19:26:48 -0400

    Projeto do curso de Django/Python
    Talvez seja necessário rafazer, devido a problemas na hora de upar

---
+++ courceProject/core/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import (
+    Marca,
+    Veiculo,
+    Pessoa, 
+    Parametro,
+    Mensalista,
+    MovRotativo,
+    MOvMensalista
+)
+
+# Register your models here.
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('entrada', 'saida', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ('mensalista', 'dt_pgto', 'total')
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
+admin.site.register(Parametro)
+admin.site.register(Mensalista)
+admin.site.register(MOvMensalista, MovMensalistaAdmin)
+admin.site.register(MovRotativo, MovRotativoAdmin)
