20e39ea6648cefc611343f9778f4d5f5c4714455
Romulo Ramos <romulo.r@samsung.com>
2019-01-16 13:13:54 -0400

    conclusao das urls/views/html para mov_rotativo e veiculos

--- courceProject/core/urls.py
+++ courceProject/core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
-from .views import home, lista_pessoas
+from .views import (
+    home, 
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos,
+)
 
 urlpatterns = [
     path('', home, name='core_home'), 
     path('pessoa', lista_pessoas, name='core_lista_pessoas'),
     path('pessoas', lista_pessoas, name='core_lista_pessoas'),
     path('veiculos', lista_veiculos, name='core_lista_veiculos'),
+    path('mov_rotativos', lista_movrotativos, name='core_lista_movrot'),
 ]
--- courceProject/core/views.py
+++ courceProject/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovRotativo,
+)
 
 # Create your views here.
 def home(request):
@@ -7,5 +11,13 @@ def home(request):
     return render(request,'core/index.html', context)
 
 def lista_pessoas(request):
-    lista = Pessoa.objects.all()
+    lista = Pessoa.objects.all() #efetua a busca no banco de todos os objetos dessa classe
     return render(request, 'core/lista_pessoas.html', {'pessoas':lista})
+
+def lista_veiculos(request):
+    lista = Veiculo.objects.all() #efetua a busca no banco de todos os objetos dessa classe
+    return render(request, 'core/lista_veiculos.html', {'veiculos':lista})
+
+def lista_movrotativos(request):
+    lista = MovRotativo.objects.all() #efetua a busca no banco de todos os objetos dessa classe
+    return render(request, 'core/lista_movRotativos.html', {'mov_rot':lista})
